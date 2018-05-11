ed9532001718bd69afbed551d197550d565438ee
JOMR92 <36526244+JOMR92@users.noreply.github.com>
2018-05-11 00:30:18 -0500

    Update plots.R

--- plots.R
+++ plots.R
@@ -27,5 +27,8 @@ f+geom_boxplot()+aes(color=type)+labs(x="year", y="log(Emissions)")
 #mirando missings por grupo
 tapply(zeroobs$fips, zeroobs$type, length)
 #plot #4
-
-
+grepled <- grepl("coal",scc$SCC.Level.Three)
+scccodes <- scc$SCC[grepled]
+head(scccodes)
+coal <- summarysccpm25[summarysccpm25$SCC %in% scccodes]
+coal2 <- subset (summarysccpm25[summarysccpm25$SCC %in% scccodes])
